7fb2fd23dbe7cd4a7918bcbf10651a9ab017986f
芦川卓矢 <ashikawatakuya@MacBook-Pro.local>
2020-09-16 11:49:55 +0900

    Userオブジェクトのタグ付け

--- app/entity/user.go
+++ app/entity/user.go
@@ -1,8 +1,12 @@
 package entity
 
+import "time"
+
 //User ユーザ
 type User struct {
-	ID    int `gorm:"primaryKey"`
-	Name  string
-	Email string
+	ID        int       `gorm:"primaryKey;autoIncrement;not null"`
+	Name      string    `gorm:"type:varchar(200);not null"`
+	Email     string    `gorm:"type:varchar(200);not null"`
+	CreatedAt time.Time `gorm:"<-:create"`
+	UpdatedAt time.Time `gorm:"<-:update"`
 }
